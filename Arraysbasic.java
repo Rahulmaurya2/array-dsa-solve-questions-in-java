7a4b0aea70484f1139b01e0540c22190b7015e26
Rahul Kumar Maurya <69010150+Rahulmaurya2@users.noreply.github.com>
2022-01-28 12:17:42 +0530

    Print and Input array in java

---
+++ Arraysbasic.java
@@ -0,0 +1,23 @@
+package DSAQ.Arrays;
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Arraysbasic {
+    public static void main (String[] args){
+
+        Scanner in = new Scanner(System.in);
+        int [] arr=new int[];
+        System.out.println("Enter the number");
+        for (int i=0;i<arr.length;i++){
+           arr[i]=in.nextInt();
+        }
+        //printArray type 1
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+        //printArray type 2
+        //System.out.println(Arrays.toString(arr));
+    }
+
+}
