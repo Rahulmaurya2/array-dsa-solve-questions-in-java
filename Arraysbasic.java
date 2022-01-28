edc9791915c0f9e48ea60d4f7928b1756f2edd6a
Rahul Kumar Maurya <69010150+Rahulmaurya2@users.noreply.github.com>
2022-01-28 12:22:42 +0530

    Update Arraysbasic.java

--- Arraysbasic.java
+++ Arraysbasic.java
@@ -7,7 +7,7 @@ public class Arraysbasic {
     public static void main (String[] args){
 
         Scanner in = new Scanner(System.in);
-        int [] arr=new int[];
+        int [] arr=new int[5];
         System.out.println("Enter the number");
         for (int i=0;i<arr.length;i++){
            arr[i]=in.nextInt();
