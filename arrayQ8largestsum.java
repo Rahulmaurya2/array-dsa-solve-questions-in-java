0fe91d1ba30d74280d32bb6f7b509b90e1a3ead1
Rahul Kumar Maurya <69010150+Rahulmaurya2@users.noreply.github.com>
2022-01-29 14:39:53 +0530

    Kadane's Algorithm for Maximum Sum Subarray

---
+++ arrayQ8largestsum.java
@@ -0,0 +1,29 @@
+package DSAQ.Arrays;
+import java.util.*;
+public class arrayQ8largestsum {
+    /**
+     * Kadane's Algorithm for Maximum Sum Sub array
+     */
+    public static void main(String[] args) {
+        int[] arr = {1};
+       // largestSum(arr);
+        System.out.println(largestSum(arr));
+    }
+    static int largestSum(int []arr){
+        int currentsum=arr[0];
+        int overallsum=arr[0];
+        for (int i = 1; i <arr.length ; i++) {
+            if (currentsum>=0){
+                currentsum+=arr[i];
+            }else {
+                currentsum=arr[i];
+            }
+            if (currentsum>overallsum){
+                overallsum=currentsum;
+            }
+
+        }
+        return overallsum;
+    }
+}
+
